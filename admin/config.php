0b00b2982cb34d098f755231b01c2c59ced937bb
extensa-tu-vladimir <webmaster@book-boutique.com>
2015-07-17 10:08:44 +0300

    installation

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/tu-demo/admin/');
+define('HTTP_CATALOG', 'http://localhost/tu-demo/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/tu-demo/admin/');
+define('HTTPS_CATALOG', 'http://localhost/tu-demo/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/tu-demo/admin/');
+define('DIR_SYSTEM', 'C:/wamp/www/tu-demo/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/tu-demo/admin/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/tu-demo/admin/view/template/');
+define('DIR_CONFIG', 'C:/wamp/www/tu-demo/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/tu-demo/image/');
+define('DIR_CACHE', 'C:/wamp/www/tu-demo/system/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/tu-demo/system/download/');
+define('DIR_UPLOAD', 'C:/wamp/www/tu-demo/system/upload/');
+define('DIR_LOGS', 'C:/wamp/www/tu-demo/system/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/tu-demo/system/modification/');
+define('DIR_CATALOG', 'C:/wamp/www/tu-demo/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'tu-demo');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
